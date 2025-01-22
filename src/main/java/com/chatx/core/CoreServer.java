8682ab3c8d5b062cbe4591b6a77a0e9c22f81fcb
ehewes <ehewes@outlook.com>
2025-01-22 17:53:31 +0000

    Current Changes, switching OS.

---
+++ src/main/java/com/chatx/core/CoreServer.java
@@ -0,0 +1,11 @@
+package src.main.java.com.chatx.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatXApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatXApplication.class, args);
+    }
+}
