d36600334c064ba392bb746cc5979c2447d78264
ehewes <ehewes@outlook.com>
2025-01-22 19:29:18 +0000

    Updated dependency, and changed configuration files

---
+++ src/main/java/com/chatx/core/ChatXApplication.java
@@ -0,0 +1,11 @@
+package com.chatx.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatXApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatXApplication.class, args);
+    }
+}
