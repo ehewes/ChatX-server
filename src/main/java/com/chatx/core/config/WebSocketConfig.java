5e18923ba7eb10ad943ba26e236a35099973788e
ehewes <ehewes@outlook.com>
2025-01-25 21:30:49 +0000

    Merge remote-tracking branch 'origin/main' into main

--- src/main/java/com/chatx/core/config/WebSocketConfig.java
+++ src/main/java/com/chatx/core/config/WebSocketConfig.java
@@ -3,12 +3,12 @@ package com.chatx.core.config;
 
 import com.chatx.core.handlers.MessageHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
